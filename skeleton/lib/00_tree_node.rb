4d7e4c37a2299102376f969bfb7ccece9af3f55e
Winston Chan <winston.chan94@gmail.com>
2018-02-16 14:25:56 -0800

    Created PolyTreeNode class with search capabilities

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,53 @@
+require "byebug"
+
 class PolyTreeNode
+  attr_reader :value, :parent, :children
+
+  def initialize(value)
+    @value = value
+    @parent = nil
+    @children = []
+  end
+
+  def parent=(node)
+    self.parent.children.delete(self) if parent
+    @parent = node
+    parent.children.push(self) if parent
+  end
+
+  def add_child(child_node)
+    child_node.parent = self
+  end
+
+  def remove_child(child_node)
+    raise "Already not a child" unless self.children.include?(child_node)
+    child_node.parent = nil
+  end
+
+  def dfs(target_value)
+    return self if self.value == target_value
+
+    self.children.each do |child|
+      search_result = child.dfs(target_value)
+      return search_result if search_result
+      next
+    end
+
+    nil
+  end
+
+  def bfs(target_value)
+    queue = [self]
+    until queue.empty?
+      node = queue.shift
+      return node if node.value == target_value
+      queue += node.children
+    end
+    nil
+  end
+
+  def inspect
+    self.to_s
+  end
 
 end
